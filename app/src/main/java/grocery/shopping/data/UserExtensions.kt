8387f573c414661e9c7d0901646cfeeafb8ce32c
Haim <haim201101@gmail.com>
2026-02-25 19:52:58 +0200

    added function to draw username

--- app/src/main/java/grocery/shopping/data/UserExtensions.kt
+++ app/src/main/java/grocery/shopping/data/UserExtensions.kt
@@ -2,7 +2,14 @@ package grocery.shopping.data
 
 import com.google.firebase.auth.FirebaseAuth
 
-fun getGoogleUser(): String? {
+fun getGoogleUserName(): String? {
+
+    val user = FirebaseAuth.getInstance().currentUser
+    val displayName = user?.displayName
+
+    return displayName
+}
+fun getGoogleUserID(): String? {
 
     val auth = FirebaseAuth.getInstance()
     val currentUid = auth.currentUser?.uid
