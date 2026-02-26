adaf5e64f52c2951f7ff97de5ac47afbd76ec2e8
Haim <haim201101@gmail.com>
2026-02-26 19:02:10 +0200

    removed unnecessary imports, added remove items function

--- app/src/main/java/grocery/shopping/creator/ListCreatorAdapter.kt
+++ app/src/main/java/grocery/shopping/creator/ListCreatorAdapter.kt
@@ -1,21 +1,22 @@
-package grocery.shopping.listmanager
+package grocery.shopping.creator
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.EditText
 import android.widget.Spinner
+import android.widget.Toast
 import androidx.core.widget.doAfterTextChanged
 import androidx.recyclerview.widget.RecyclerView
 import grocery.shopping.R
 import grocery.shopping.data.CompleteShoppingList
-import grocery.shopping.data.DEFAULT_ID
 import grocery.shopping.data.DEFAULT_ITEM_QUANTITY
 import grocery.shopping.data.DEFAULT_LIST_NAME
 import grocery.shopping.data.DEFAULT_PRODUCT_NAME
-import grocery.shopping.data.GENERAL_TYPE
 import grocery.shopping.data.GroceryItems
 import grocery.shopping.data.MAX_ITEM_QUANTITY
 import grocery.shopping.data.MIN_ITEM_QUANTITY
@@ -84,26 +85,58 @@ class ListCreatorAdapter : RecyclerView.Adapter<ListCreatorAdapter.ItemViewHolde
             override fun onNothingSelected(parent: AdapterView<*>?) {}
         }
     }
-
-
+   // return the number of items in the list
     override fun getItemCount(): Int {
-        // Return the number of items in the list
+
         return listOfProducts.size
     }
 
-    fun addNewItem() {
-        // Add a new item to the list
-        listOfProducts.add(
-            GroceryItems(
-                DEFAULT_ID,
-                GENERAL_TYPE,
-                DEFAULT_PRODUCT_NAME,
-                DEFAULT_ITEM_QUANTITY
+    // Add a new item to the list
+    fun addNewItem(recyclerView: RecyclerView) {
+
+        if(listOfProducts.isEmpty() || listOfProducts[listOfProducts.size-1].name.isNotBlank()) {
+
+            listOfProducts.add(
+                GroceryItems(
+                    name = DEFAULT_PRODUCT_NAME,
+                    quantity = DEFAULT_ITEM_QUANTITY
+                )
             )
-        )
-        notifyItemInserted(listOfProducts.size - 1)
+            // Notify the adapter that an item has been added
+            notifyItemInserted(listOfProducts.size - 1)
+
+            useKeyboardForNextLIne(recyclerView)
+        }
+        else{
+            Toast.makeText(recyclerView.context, "קודם יש לכתוב בשורה הזאת", Toast.LENGTH_SHORT).show()
+        }
+    }
+    // make keyboard show for the new line in recyclerview
+    fun useKeyboardForNextLIne(recyclerView: RecyclerView){
+        // Scroll to the new item
+        recyclerView.scrollToPosition(listOfProducts.size-1)
+        recyclerView.post {
+            // refence to the last item in the list
+            val viewHolder = recyclerView.findViewHolderForAdapterPosition(listOfProducts.size-1)
+            //reference to the edit text in the last item
+            val editText = viewHolder?.itemView?.findViewById<EditText>(R.id.productName)
+            editText?.let {
+                // Set focus on the EditText
+                it.requestFocus()
+                // Show the keyboard
+                val imm = recyclerView.context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                imm.showSoftInput(it, InputMethodManager.SHOW_IMPLICIT)
+            }
+        }
+
     }
 
+    fun removeItem(position: Int) {
+        if (listOfProducts.isNotEmpty()){
+        listOfProducts.removeAt(position)
+        notifyItemRemoved(position)
+    }
+        }
     fun saveItems(){
 
         for (item in listOfProducts) {
@@ -116,7 +149,6 @@ class ListCreatorAdapter : RecyclerView.Adapter<ListCreatorAdapter.ItemViewHolde
             val shoppingList = CompleteShoppingList(
                 _listName = DEFAULT_LIST_NAME,
                 _items = finalSortedList,
-                _userIdCreator = getGoogleUserName(), _timeCreated = System.currentTimeMillis()
             )
             ShoppingRepository.saveList(shoppingList)
             //notifyDataSetChanged()
@@ -127,15 +159,19 @@ class ListCreatorAdapter : RecyclerView.Adapter<ListCreatorAdapter.ItemViewHolde
                 println("Item: ${item.name}, Quantity: ${item.quantity},username: ${getGoogleUserName()}")
             }
     }
-
+    // check if the list is valid
     fun isDataValid(listOfProducts: MutableList<GroceryItems>): Boolean {
 
-        return !listOfProducts.isEmpty() && !listOfProducts.all { it.name.isBlank() }
+        return listOfProducts.isNotEmpty() && listOfProducts.all {it.name.isNotBlank()}
     }
 
+    // check if the list was saved
     fun wasSaved() : Boolean{
 
         return (isDataValid(listOfProducts))
     }
+
+
+
 
 }
