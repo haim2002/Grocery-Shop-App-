3acac5159f40bf46cdb725e9f5226a1460d73111
Haim <haim201101@gmail.com>
2026-02-26 19:00:47 +0200

    added back button and made recyclerView outside class

--- app/src/main/java/grocery/shopping/creator/ListCreator.kt
+++ app/src/main/java/grocery/shopping/creator/ListCreator.kt
@@ -1,4 +1,4 @@
-package grocery.shopping.listmanager
+package grocery.shopping.creator
 
 import android.os.Bundle
 import android.view.Menu
@@ -13,18 +13,22 @@ import androidx.recyclerview.widget.RecyclerView
 import grocery.shopping.R
 
 private lateinit var myAdapter: ListCreatorAdapter
+private lateinit var recyclerView: RecyclerView
+
 class ListCreator : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.list_creator)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.list_maker)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+
         }
         // Set up the RecyclerView
-        val recyclerView: RecyclerView = findViewById(R.id.recyclerView)
+        recyclerView= findViewById(R.id.recyclerView)
 
         // Initialize the adapter
         myAdapter = ListCreatorAdapter()
@@ -40,23 +44,36 @@ class ListCreator : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
         // Check which item was clicked
         return when (item.itemId) {
-            R.id.addListItem -> {
-                myAdapter.addNewItem()
+            // Handle the home button click
+            android.R.id.home -> {
+                // This acts like the hardware back button
+                onBackPressedDispatcher.onBackPressed()
                 true
             }
+            // Handle adding list item
+            R.id.addListItem -> {
+                myAdapter.addNewItem(recyclerView)
+                true
+            }
+            // Handle saving list
             R.id.saveItems -> {
 
                 myAdapter.saveItems()
-
+                // validating if the list was saved
                 val wasSaved = myAdapter.wasSaved()
                 if (wasSaved) {
-                    finish()
+                    //finish()
                 }
                 else{
                     Toast.makeText(this, "אי אפשר לשמור רשימה ריקה", Toast.LENGTH_SHORT).show()
                 }
+                true
+            }
+            R.id.deleteListItem -> {
+                myAdapter.removeItem(recyclerView.getChildAdapterPosition(recyclerView.focusedChild))
                 true
             }
             else -> super.onOptionsItemSelected(item)
