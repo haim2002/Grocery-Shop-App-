1cca385e67675b20fd7b0cf020cfd6aadc537d61
Haim <haim201101@gmail.com>
2026-03-07 14:45:24 +0200

    new activity that meant to display the list of groceries after clicking on the list in main

---
+++ app/src/main/java/grocery/shopping/ListDisplayAdapter.kt
@@ -0,0 +1,51 @@
+package grocery.shopping
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import grocery.shopping.data.GENERAL_TYPE
+import grocery.shopping.data.GroceryItems
+
+class ListDisplayAdapter : RecyclerView.Adapter<ListDisplayAdapter.ItemViewHolder>() {
+
+    val listOfProducts: MutableList<GroceryItems> = mutableListOf()
+
+    init {
+
+        listOfProducts.add(GroceryItems(GENERAL_TYPE, "קקק", 8))
+        listOfProducts.add(GroceryItems(GENERAL_TYPE, "בשדב", 5))
+
+    }
+
+    class ItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        //reference to the views in the layout
+        val productName: TextView = itemView.findViewById(R.id.product_display)
+        val productAmount: TextView = itemView.findViewById(R.id.quantity_display)
+    }
+
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): ItemViewHolder { //inflate the layout of each item
+        val inflatedView: View = LayoutInflater.from(parent.context)
+            .inflate(R.layout.list_display_adapter, parent, false)
+        return ItemViewHolder(inflatedView)
+    }
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        // make reference of current item in the list
+        val currentItem = listOfProducts[position]
+        holder.productName.text = currentItem.name
+        holder.productAmount.text = currentItem.quantity.toString()
+    }
+
+    override fun getItemCount(): Int {
+
+        return listOfProducts.size
+    }
+
+
+}
---
+++ app/src/main/java/grocery/shopping/ListDisplayer.kt
@@ -0,0 +1,34 @@
+package grocery.shopping
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+private lateinit var listAdapter: ListDisplayAdapter
+private lateinit var recyclerView: RecyclerView
+class ListDisplayer : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.list_creator)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.list_maker)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+
+        recyclerView = findViewById(R.id.recyclerView)
+
+        // Initialize the adapter
+        listAdapter = ListDisplayAdapter()
+
+        // Set the adapter and layout manager for the RecyclerView
+        recyclerView.adapter = listAdapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
+    }
+}
