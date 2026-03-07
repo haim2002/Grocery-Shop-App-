fafad9f36385149764381120782376ac0d477a1c
Haim <haim201101@gmail.com>
2026-03-07 14:28:12 +0200

    added val for firebase key and creator name

--- app/src/main/java/grocery/shopping/data/DataStructures.kt
+++ app/src/main/java/grocery/shopping/data/DataStructures.kt
@@ -1,29 +1,41 @@
 package grocery.shopping.data
 
-data class CompleteShoppingList(
-        private var _listName: String,
-        private var _items: MutableList<GroceryItems> = mutableListOf(),
-        private val _timeCreated: Long = System.currentTimeMillis(),
-        private var _userIdLastEdited: String? = null
-    ) {
-        // Getters and Setters
+data class ListInfo(
+    private var _listName: String?,
+   // private var _items: MutableList<GroceryItems>,
+    private val _timeCreated: Long = System.currentTimeMillis(),
+    private val _creatorName: String,
+    private var _userIdLastEdited: String? = null,
+    private val _firebaseKey: String? = null
+) {
+    // Getters and Setters
 
-        var listName: String
-            get() = _listName
-            set(value) {
-                _listName = value.ifBlank {
-                    DEFAULT_LIST_NAME
-                }
+    var listName: String?
+        get() = _listName
+        set(value) {
+            _listName = value?.ifBlank {
+                DEFAULT_LIST_NAME
             }
+        }
+/*
+    var items: MutableList<GroceryItems>
+        get() = _items
+        set(value) {
+            _items = value
+        }
+*/
+    val dateCreatedTime: Long
+        get() = _timeCreated
 
-        var items: MutableList<GroceryItems>
-            get() = _items
-            set(value) { _items = value }
+    var firebaseKey: String? = null
+        get() = _firebaseKey
 
-        val dateCreated: Long
-            get() = _timeCreated
+    val creatorUserName: String
+        get() = _creatorName
 
-        var userIdLastEdited: String?
-            get() = _userIdLastEdited
-            set(value) { _userIdLastEdited = value }
-    }
+    var userIdLastEdited: String?
+        get() = _userIdLastEdited
+        set(value) {
+            _userIdLastEdited = value
+        }
+}
