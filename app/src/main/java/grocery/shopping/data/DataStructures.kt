82079c3daa98e6e2a9577ad0fe0e3fcecd2a1e3a
Haim <haim201101@gmail.com>
2026-03-08 23:12:01 +0200

    removed private for easer use

--- app/src/main/java/grocery/shopping/data/DataStructures.kt
+++ app/src/main/java/grocery/shopping/data/DataStructures.kt
@@ -1,41 +1,13 @@
 package grocery.shopping.data
 
 data class ListInfo(
-    private var _listName: String?,
-   // private var _items: MutableList<GroceryItems>,
-    private val _timeCreated: Long = System.currentTimeMillis(),
-    private val _creatorName: String,
-    private var _userIdLastEdited: String? = null,
-    private val _firebaseKey: String? = null
-) {
-    // Getters and Setters
+    var listName: String= UNNAMED_LIST,
+    val timeCreated: Long = System.currentTimeMillis(),
+    var creatorName: String= "bbb",
+    var userIdLastEdited: String = "null",
+    var firebaseKey: String = "null"
+)
 
-    var listName: String?
-        get() = _listName
-        set(value) {
-            _listName = value?.ifBlank {
-                DEFAULT_LIST_NAME
-            }
-        }
-/*
-    var items: MutableList<GroceryItems>
-        get() = _items
-        set(value) {
-            _items = value
-        }
-*/
-    val dateCreatedTime: Long
-        get() = _timeCreated
 
-    var firebaseKey: String? = null
-        get() = _firebaseKey
 
-    val creatorUserName: String
-        get() = _creatorName
 
-    var userIdLastEdited: String?
-        get() = _userIdLastEdited
-        set(value) {
-            _userIdLastEdited = value
-        }
-}
