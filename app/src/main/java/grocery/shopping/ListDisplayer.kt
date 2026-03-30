d9b11ed9cfd54b4b01cacfb36c1fccc88187d122
Haim <haim201101@gmail.com>
2026-03-30 18:59:13 +0300

    shows the name of the list that's being edited

--- app/src/main/java/grocery/shopping/ListDisplayer.kt
+++ app/src/main/java/grocery/shopping/ListDisplayer.kt
@@ -1,6 +1,5 @@
 package grocery.shopping
 
-import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
@@ -20,6 +19,7 @@ private lateinit var listAdapter: ListDisplayAdapter
 private lateinit var recyclerView: RecyclerView
 private lateinit var listId: String
 
+
 class ListDisplayer : AppCompatActivity() {
     val startForResult =
         registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
@@ -31,7 +31,7 @@ class ListDisplayer : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+     //   enableEdgeToEdge()
         setContentView(R.layout.list_creator)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.list_maker)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
@@ -40,6 +40,7 @@ class ListDisplayer : AppCompatActivity() {
 
         }
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.title = intent.getStringExtra("LIST_NAME")
         recyclerView = findViewById(R.id.recyclerView)
 
         // Initialize the adapter
@@ -47,7 +48,9 @@ class ListDisplayer : AppCompatActivity() {
         Log.d("ListDisplayerID", "listId: $listId")
 
         fetchGroceryList(listId) { listFromFirebase ->
-            listAdapter = ListDisplayAdapter(listFromFirebase)
+            listAdapter = ListDisplayAdapter(listFromFirebase, listId)
+            Log.d("ListDisplayerDebug", "listId: ${listId}")
+
             // Set the adapter and layout manager for the RecyclerView
             recyclerView.adapter = listAdapter
             recyclerView.layoutManager = LinearLayoutManager(this)
@@ -75,7 +78,7 @@ class ListDisplayer : AppCompatActivity() {
 
             R.id.editList -> {
 
-                Log.d("ListDisplayerID2", "listId: $listId")
+                Log.d("ListDisplayerID2", "listId: ${listId}")
                 val intent = Intent(this, ListCreator::class.java)
                 intent.putExtra("ListID", listId)
                 startForResult.launch(intent)
@@ -92,8 +95,8 @@ class ListDisplayer : AppCompatActivity() {
 
 
         fetchGroceryList(listId) { listFromFirebase ->
-            listAdapter = ListDisplayAdapter(listFromFirebase)
-            Log.d("ListDisplayerDebug", "listId: $listId")
+            listAdapter = ListDisplayAdapter(listFromFirebase, listId)
+            Log.d("ListDisplayerDebug", "listId: ${listId}")
             Log.d("ListDisplayerDebug", "listFromFirebase: $listFromFirebase")
             // Set the adapter and layout manager for the RecyclerView
             recyclerView.adapter = listAdapter
@@ -103,4 +106,6 @@ class ListDisplayer : AppCompatActivity() {
         }
 
     }
+
+
 }
