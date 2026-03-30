829906bf49cb5c9b32ec6719b5084b1654d4c3b9
Haim <haim201101@gmail.com>
2026-03-30 18:53:38 +0300

    new default values

--- app/src/main/java/grocery/shopping/data/DataStructures.kt
+++ app/src/main/java/grocery/shopping/data/DataStructures.kt
@@ -3,8 +3,8 @@ package grocery.shopping.data
 data class ListInfo(
     var listName: String = UNNAMED_LIST,
     var timeCreated: Long = System.currentTimeMillis(),
-    var createdBy: String = "bbb",
-    var updatedBy: String = "null",
+    var createdBy: String = DEFAULT_USER_NAME,
+    var updatedBy: String = DEFAULT_USER_NAME,
     var firebaseKey: String = "null"
 )
 
